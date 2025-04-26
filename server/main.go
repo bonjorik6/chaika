dd053a8c69760126bbf2839beb49187c442c891d
bonjorik6 <vasilina6925@gmail.com>
2025-04-26 23:44:23 +0300

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,61 +1,67 @@
-// server.go
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"os"
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool { return true },
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
 var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan []byte)
 
 func main() {
-    http.HandleFunc("/ws", handleConnections)
+	http.HandleFunc("/ws", handleConnections)
 
-    go handleMessages()
+	go handleMessages()
 
-    log.Println("Server started on :8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // локально
+	}
+
+	log.Println("Server started on port:", port)
+	err := http.ListenAndServe(":" + port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-    ws, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer ws.Close()
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ws.Close()
 
-    clients[ws] = true
+	clients[ws] = true
 
-    for {
-        _, msg, err := ws.ReadMessage()
-        if err != nil {
-            log.Println("error:", err)
-            delete(clients, ws)
-            break
-        }
-        broadcast <- msg
-    }
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("error:", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
 }
 
 func handleMessages() {
-    for {
-        msg := <-broadcast
-        for client := range clients {
-            err := client.WriteMessage(websocket.TextMessage, msg)
-            if err != nil {
-                log.Printf("error: %v", err)
-                client.Close()
-                delete(clients, client)
-            }
-        }
-    }
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
