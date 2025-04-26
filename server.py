1584801acc3e39b5a23ed9a0d1a08967f1ad3658
bonjorik6 <vasilina6925@gmail.com>
2025-04-27 01:40:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
 import asyncio
 import websockets
-import os
 
 connected_clients = set()
 
-async def handler(websocket, path):
+async def handler(websocket):
     connected_clients.add(websocket)
     try:
         async for message in websocket:
-            await asyncio.gather(*[client.send(message) for client in connected_clients])
-    except websockets.ConnectionClosed:
+            await asyncio.gather(
+                *[client.send(message) for client in connected_clients if client != websocket]
+            )
+    except websockets.ConnectionClosedOK:
         print("Клиент отключился")
+    except Exception as e:
+        print(f"Ошибка: {e}")
     finally:
         connected_clients.remove(websocket)
 
 async def main():
-    port = int(os.environ.get("PORT", 8080))
-    async with websockets.serve(handler, "0.0.0.0", port):
-        print(f"Сервер запущен на порту {port}")
+    async with websockets.serve(handler, "0.0.0.0", 8080):
+        print("Сервер запущен на порту 8080")
         await asyncio.Future()
 
 if __name__ == "__main__":
